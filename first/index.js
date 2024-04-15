74a21c1abc869a5f81c17230c73385a5f9421ccb
jiyongjin12 <l1110hj@gmail.com>
2024-04-15 14:03:05 +0900

    #image

--- first/index.js
+++ first/index.js
@@ -1,11 +1,10 @@
-
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d"); //2d로 만들겠다는 코드
 
 canvas.width = 1024;
 canvas.height = 576;
 
-const gravity = 0.2;
+const gravity = 0.5;
 
 const background = new Sprite({
     position : {
@@ -13,6 +12,16 @@ const background = new Sprite({
         y : 0,
     },
     imageSrc : "/first/img/background.png",
+})
+
+const shop = new Sprite({
+    position : {
+        x : 600,
+        y : 128,
+    },
+    imageSrc : "/first/img/shop.png",
+    scale : 2.75,
+    framesMax : 6,
 })
 
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -85,6 +94,7 @@ function animate(){
     c.fillRect(0, 0, canvas.width, canvas.height);
 
     background.update();
+    shop.update();
     
     player.update();
     enemy.update();
@@ -152,7 +162,7 @@ window.addEventListener("keydown", (event) => {
         case "w":
             player.velocity.y = -10;
             break;
-        case " ":
+        case "s":
             player.attack();
             break;
         case "ArrowRight":
